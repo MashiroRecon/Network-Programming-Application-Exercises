5616bef4d2e050a27c3ba1f27c018631847d3e27
Mashiro_Recon <takuyuki.kurorekishi@gmail.com>
2022-11-30 13:43:50 +0900

    Do report02/2.

--- src/Report02/Rect.java
+++ src/Report02/Rect.java
@@ -6,6 +6,10 @@ public class Rect extends Figure {
     Rect(){}
     @Override public void paint(Graphics g){
         g.setColor(color);
-        g.drawRect(x,y,w,h);
+        if(w>=0&&h>=0)g.drawRect(x,y,w,h);
+        else if(w<0&&h>=0)g.drawRect(x+w,y,-w,h);
+        else if(w>=0&&h<0)g.drawRect(x,y+h,w,-h);
+        else if(w<0&&h<0)g.drawRect(x+w,y+h,-w,-h);
+        else g.drawRect(x,y,w,h);
     }
 }
