7f63af6682ed563d24407e9129127836de557507
Mashiro_Recon <takuyuki.kurorekishi@gmail.com>
2022-12-06 16:25:17 +0900

    Minor correction.

--- src/Report02/RedZone.java
+++ src/Report02/RedZone.java
@@ -2,6 +2,10 @@ package Report02;
 
 import java.awt.*;
 import javax.swing.*;
+import java.io.*;
+import java.net.MalformedURLException;
+import javax.sound.sampled.*;
+
 
 public class RedZone {
     public static void redzone(){
@@ -17,9 +21,45 @@ public class RedZone {
         int first=JOptionPane.showConfirmDialog(dialog,fmessage,"RED ZONE", JOptionPane.OK_CANCEL_OPTION);
         if(first==JOptionPane.OK_OPTION){
             int second=JOptionPane.showConfirmDialog(dialog,smessage,"Final Confirmation", JOptionPane.OK_CANCEL_OPTION);
-            if(second==JOptionPane.OK_OPTION){}
+            if(second==JOptionPane.OK_OPTION){
+                Clip bgm=createClip(new File("RedZone.wav"));
+                bgm.start();
+                try {
+                    Thread.sleep(3000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                FloatControl ctrl=(FloatControl)bgm.getControl(FloatControl.Type.MASTER_GAIN);
+                ctrl.setValue(0);
+                bgm.close();
+            }
             else if(second==JOptionPane.CANCEL_OPTION){}
         }
         else if(first==JOptionPane.CANCEL_OPTION){}
     }
+    public static Clip createClip(File path){
+        try(AudioInputStream ais=AudioSystem.getAudioInputStream(path)){
+            AudioFormat af=ais.getFormat();
+            DataLine.Info dataLine=new DataLine.Info(Clip.class,af);
+            Clip c=(Clip)AudioSystem.getLine(dataLine);
+            c.open(ais);
+            return c;
+        }
+        catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch(UnsupportedAudioFileException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(LineUnavailableException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
